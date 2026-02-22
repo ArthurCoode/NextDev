d6e68c70b8b2663754974abe175f6fe90cfe8dfb
Arthur <you@example.com>
2026-02-22 12:08:06 -0300

    atualizando index, retirando sorteio

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,7 +1,6 @@
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import About from "@/components/About";
-import Sorteio from "@/components/sorteio";
 import Services from "@/components/Services";
 import Portfolio from "@/components/Portfolio";
 import Contact from "@/components/Contact";
